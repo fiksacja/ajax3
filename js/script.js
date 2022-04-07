7e384cd80d4ebf19f371879c57569fb51c08d196
Katarzyna Lesiak <katarzyna.lesiak@medius.com>
2022-04-07 13:09:31 +0200

    3-pobierz-dane-po-kliknieciu-przycisku-jquery

--- js/script.js
+++ js/script.js
@@ -6,39 +6,39 @@ $(document).ready(function () {
 
         // wariant 1 za pomoca GET
 
-        /*        $.get('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js')
-                   .done(function(data) {
-       
-                       let pId = $('<p></p>').text(`Post ID: ${data.id}`);
-                       let pUserId = $('<p></p>').text(`User ID: ${data.userId}`);
-                       let pTitle = $('<p></p>').text(`Title: ${data.title}`);
-                       let pBody = $('<p></p>').text(`Body: ${data.body}`);
-                       let hr = $('<hr />');
-       
-                       let jqBody = $('body');
-                       
-                       jqBody.append(pId);
-                       jqBody.append(pUserId);
-                       jqBody.append(pTitle);
-                       jqBody.append(pBody);
-                       jqBody.append(hr);
-       
-                    //   console.log(pId);
-                   })
-       
-                   .fail(function(error) {
-                       console.error(error);
-       
-       
-                   });
-           });
-       
-       });
-        */
+        /*         $.get('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js')
+                    .done(function(data) {
+        
+                        let pId = $('<p></p>').text(`Post ID: ${data.id}`);
+                        let pUserId = $('<p></p>').text(`User ID: ${data.userId}`);
+                        let pTitle = $('<p></p>').text(`Title: ${data.title}`);
+                        let pBody = $('<p></p>').text(`Body: ${data.body}`);
+                        let hr = $('<hr />');
+        
+                        let jqBody = $('body');
+                        
+                        jqBody.append(pId);
+                        jqBody.append(pUserId);
+                        jqBody.append(pTitle);
+                        jqBody.append(pBody);
+                        jqBody.append(hr);
+        
+                     //   console.log(pId);
+                    })
+        
+                    .fail(function(error) {
+                        console.error(error);
+        
+        
+                    });
+            });
+        
+        }); */
+
         // wariant 2 z metoda GET JSON
 
-        $.getJSON('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js')
-            .done(function (data) {
+        $.getJSON('https://akademia108.pl/api/ajax/get-post.php')
+            .done(function(data) {
 
                 let pId = $('<p></p>').text(`Post ID: ${data.id}`);
                 let pUserId = $('<p></p>').text(`User ID: ${data.userId}`);
@@ -58,7 +58,7 @@ $(document).ready(function () {
             })
 
             .fail(function (error) {
-                console.error(error);
+                console.log(error);
 
 
             });
